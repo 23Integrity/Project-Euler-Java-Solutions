278e45fcbb839296c30aecc28b972c77322d47e3
Michał Wesoły <mkwesolt@gmail.com>
2021-03-12 12:56:57 +0100

    Solution to task 1

--- Task1/src/Task1.java
+++ Task1/src/Task1.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String... args) {
-        
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        System.out.println(FindSumOfMultiplesOf3And5(value));
+    }
+
+    static int FindSumOfMultiplesOf3And5(int max) {
+        int sum = 0;
+        for (int i = 1; i < max; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
